24bcc4c2daad555e507c9058e47cf4f1e55a9a9f
jossvo <jossvo26@outlook.com>
2022-11-21 19:47:30 +0000

    Commit for adding excuse generator in JS file

--- src/app.js
+++ src/app.js
@@ -7,5 +7,29 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  let whoR = Math.floor(Math.random() * 4);
+  let actionR = Math.floor(Math.random() * 4);
+  let whatR = Math.floor(Math.random() * 4);
+  let whenR = Math.floor(Math.random() * 5);
+
+  let respone =
+    who[whoR.toString()] +
+    " " +
+    action[actionR.toString()] +
+    " " +
+    what[whatR.toString()] +
+    " " +
+    when[whenR.toString()];
+
+  document.getElementById("excuse").innerHTML = respone;
 };
